2d26e247bd757968b44461ba34a45b92932614c8
abduseK <abdelselamkemal74@gmail.com>
2024-03-09 22:58:36 -0800

    update on intro and about components

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,9 +65,8 @@ export default function Intro() {
       >
         <span className="font-bold">Hey there, I'm Abdu.</span> I'm a{" "}
         <span className="font-bold">full-stack developer and video editor</span> with{" "}
-        <span className="font-bold">4 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">4 years</span> of experience<span className="italic"></span>. Feel free to explore{" "}
+        
       </motion.h1>
 
       <motion.div
@@ -92,7 +91,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/resume.pdf"
           download
         >
           Download CV{" "}
@@ -101,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://linkedin.com/in/ab-adam74"
           target="_blank"
         >
           <BsLinkedin />
@@ -109,7 +108,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/abduseK"
           target="_blank"
         >
           <FaGithubSquare />
